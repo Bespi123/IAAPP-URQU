1a8344d9a48b64bf017481ad4349774a959cd7eb
bespi123 <bespinozag@unsa.edu.pe>
2022-08-09 14:57:02 -0500

    Update functions to read Gamit files and SLR files

--- ReadGamitFiles.m
+++ ReadGamitFiles.m
@@ -2,28 +2,6 @@ function station = ReadGamitFiles(fileName)
 
 fileID = fopen(fileName);
 
-%% Read Header
-fseek(fileID,80,'bof');
-tline = fgetl(fileID);
-station.name = sscanf(tline,'4-character ID: %s', 1);
-fseek(fileID,134,'bof');
-tline = fgetl(fileID);
-station.firstEpoch = sscanf(tline,'First Epoch   : %d %d', [1 2]);
-tline = fgetl(fileID);
-station.lastEpoch = sscanf(tline,'Last Epoch    : %d %d', [1 2]);
-tline = fgetl(fileID);
-station.releaseDate = sscanf(tline,'Release Date  : %d %d', [1 2]);
-tline = fgetl(fileID);
-temp = sscanf(tline,'XYZ Reference position :   %f %f %f (%s)', [1 4]);
-station.xyzReferencePosition = temp(1:3); 
-station.xyzReferencePositionRef =char(temp(4:end-1));
-tline = fgetl(fileID);
-temp = sscanf(tline,'NEU Reference position :   %f %f %f (%s)', [1 4]);
-station.neuReferencePosition = temp(1:3); 
-station.neuReferencePositionRef =char(temp(4:end-1));
-fseek(fileID,2281,'bof');
-tline = fgetl(fileID);
-
 %% Data Containers
 station.YYYYMMDD=[];          %Year, month, day for the given position epoch
 station.HHMMSS=[];            %Hour, minute, second for the given position epoch
@@ -50,35 +28,53 @@ station.Rne=[];           %Correlation of dN and dE
 station.Rnu=[];           %Correlation of dN and dU
 station.Reu=[];           %Correlation of dEand dU
 
-%% Read files
-while(tline>0)
-    temp = sscanf(tline,[' %d %d %f  %f %f %f  %f  %f  %f %f %f  %f' ...
-        '     %f  %f %f     %f   %f  %f    %f  %f  %f  %f %f  %f orbit'], [1 24]);
-    station.YYYYMMDD=[station.YYYYMMDD;temp(1)]; 
-    station.HHMMSS=[station.HHMMSS;temp(2)];
-    station.modifiedJulianDay = [station.modifiedJulianDay ;temp(3)];
-    station.X=[station.X;temp(4)];
-    station.Y=[station.Y;temp(5)];        
-    station.Z=[station.Z;temp(6)];         
-    station.Sx=[station.Sx;temp(7)];
-    station.Sy=[station.Sy;temp(8)]; 
-    station.Sz=[station.Sz;temp(9)];
-    station.Rxy=[station.Rxy;temp(10)];
-    station.Rxz=[station.Rxz;temp(11)];
-    station.Ryz=[station.Ryz;temp(12)];
-    station.Nlat=[station.Nlat;temp(13)];
-    station.Elong=[station.Elong;temp(14)];         %East longitude, WGS-84 ellipsoid, decimal degrees
-    station.HeightUp=[station.HeightUp;temp(15)];      %Height relative to WGS-84 ellipsoid, m
-    station.dN =[station.dN;temp(16)];           %Difference in North component from NEU reference position, meters
-    station.dE =[station.dE;temp(17)];           %Difference in East component from NEU reference position, meters
-    station.du =[station.du;temp(18)];           %Difference in vertical component from NEU reference position, meters
-    station.Sn =[station.Sn;temp(19)];           %Standard deviation of dN, meters
-    station.Se =[station.Se;temp(20)];           %Standard deviation of dE, meters
-    station.Su =[station.Su;temp(21)];           %Standard deviation of dU, meters
-    station.Rne=[station.Rne;temp(22)];           %Correlation of dN and dE
-    station.Rnu=[station.Rnu;temp(23)];           %Correlation of dN and dU
-    station.Reu=[station.Reu;temp(24)];           %Correlation of dEand dU
-    tline = fgetl(fileID);
+while ~feof(fileID)
+   tline = fgetl(fileID);
+   if (strcmp(tline(1:11),'4-character'))
+        station.name = sscanf(tline,'4-character ID: %s', 1);
+   elseif (strcmp(tline(1:11),'First Epoch'))
+        station.firstEpoch = sscanf(tline,'First Epoch   : %d %d', [1 2]);
+        tline = fgetl(fileID);
+        station.lastEpoch = sscanf(tline,'Last Epoch    : %d %d', [1 2]);
+        tline = fgetl(fileID);
+        station.releaseDate = sscanf(tline,'Release Date  : %d %d', [1 2]);
+        tline = fgetl(fileID);
+        temp = sscanf(tline,'XYZ Reference position :   %f %f %f (%s)', [1 4]);
+        station.xyzReferencePosition = temp(1:3); 
+        station.xyzReferencePositionRef =char(temp(4:end-1));
+        tline = fgetl(fileID);
+        temp = sscanf(tline,'NEU Reference position :   %f %f %f (%s)', [1 4]);
+        station.neuReferencePosition = temp(1:3); 
+        station.neuReferencePositionRef =char(temp(4:end-1));
+   elseif (strcmp(tline(end-4:end),'orbit')||strcmp(tline(end-4:end),'final'))
+        temp = sscanf(tline,[' %d %d %f  %f %f %f  %f  %f  %f %f %f  %f' ...
+            '     %f  %f %f     %f   %f  %f    %f  %f  %f  %f %f  %f orbit'], [1 24]);
+        station.YYYYMMDD=[station.YYYYMMDD;temp(1)]; 
+        station.HHMMSS=[station.HHMMSS;temp(2)];
+        station.modifiedJulianDay = [station.modifiedJulianDay ;temp(3)];
+        station.X=[station.X;temp(4)];
+        station.Y=[station.Y;temp(5)];        
+        station.Z=[station.Z;temp(6)];         
+        station.Sx=[station.Sx;temp(7)];
+        station.Sy=[station.Sy;temp(8)]; 
+        station.Sz=[station.Sz;temp(9)];
+        station.Rxy=[station.Rxy;temp(10)];
+        station.Rxz=[station.Rxz;temp(11)];
+        station.Ryz=[station.Ryz;temp(12)];
+        station.Nlat=[station.Nlat;temp(13)];
+        station.Elong=[station.Elong;temp(14)];       %East longitude, WGS-84 ellipsoid, decimal degrees
+        station.HeightUp=[station.HeightUp;temp(15)]; %Height relative to WGS-84 ellipsoid, m
+        station.dN =[station.dN;temp(16)];            %Difference in North component from NEU reference position, meters
+        station.dE =[station.dE;temp(17)];            %Difference in East component from NEU reference position, meters
+        station.du =[station.du;temp(18)];            %Difference in vertical component from NEU reference position, meters
+        station.Sn =[station.Sn;temp(19)];            %Standard deviation of dN, meters
+        station.Se =[station.Se;temp(20)];            %Standard deviation of dE, meters
+        station.Su =[station.Su;temp(21)];            %Standard deviation of dU, meters
+        station.Rne=[station.Rne;temp(22)];           %Correlation of dN and dE
+        station.Rnu=[station.Rnu;temp(23)];           %Correlation of dN and dU
+        station.Reu=[station.Reu;temp(24)];           %Correlation of dEand dU
+   end
 end
+
 fclose(fileID);
 end
---
+++ targetsExperiments.m
@@ -0,0 +1,155 @@
+%Clear and close files
+clear, clc, close all
+
+%% Read TRACK files
+%%%Read files for minico test
+taraGEOD = readTrack('TRAK2040GEODtaraLC.csv');
+tarbGEOD = readTrack('TRAK2040GEODtarbLC.csv');
+tarcGEOD = readTrack('TRAK2040GEODtarcLC.csv');
+aregGEOD = readTrack('TRAK2040GEODAREGLC.csv');
+taraNEU = readTrack('TRAK2040NEUtaraLC.csv');
+tarbNEU = readTrack('TRAK2040NEUtarbLC.csv');
+tarcNEU = readTrack('TRAK2040NEUtarcLC.csv');
+
+%% Read Gamit files
+tara = ReadGamitFiles('TARA.iaa.orbit_igs14.pos');
+tarb = ReadGamitFiles('TARB.iaa.orbit_igs14.pos');
+
+%% Read Laser files
+%Read .sm files
+smFiles = dir('Data/Laser/*.sm');
+for i=1:length(smFiles)
+    minico(i)=readTTsmfiles(smFiles(i).name);
+end
+
+%Read Raw data
+ laserRawData=readLaserData('loge_y2021d215t171131');
+%Read Proseced data
+  
+
+
+function site = readTrack(file)
+    data = readmatrix(file);
+    [a,b] = size(data);
+    if (b==18)
+        %disp('GEOD data detected');
+        site.year=data(:,1);
+        site.doy=data(:,2);
+        site.seconds=data(:,3);
+        site.latitude=data(:,4);
+        site.longitude=data(:,5);
+        site.height=data(:,6);
+        site.sigN=data(:,7);
+        site.sigN=data(:,8);
+        site.sigE=data(:,9);
+        site.sigH=data(:,10);
+        site.michi=data(:,11);
+        site.atm=data(:,12);
+        site.sigAtm=data(:,13);
+        site.FracDoy=data(:,14);
+        site.Epoch=data(:,15);
+        site.BF=data(:,16);
+        site.Not=data(:,17);
+    elseif (b==22)
+        %disp('NEU data detected');
+        site.year=data(:,1);
+        site.month=data(:,2);
+        site.day=data(:,3);
+        site.hour=data(:,4);
+        site.min=data(:,5);
+        site.secconds=data(:,6);
+        site.dN=data(:,7);
+        site.sigN=data(:,8);
+        site.dE=data(:,9);
+        site.sigE=data(:,10);
+        site.dU=data(:,11);
+        site.sigU=data(:,12);
+        site.RMS=data(:,13);
+        site.michi=data(:,14);
+        site.atm=data(:,15);
+        site.sigAtm=data(:,16);
+        site.fracDoy=data(:,17);
+        site.epoch=data(:,18);
+        site.michiB=data(:,19);
+        site.no=data(:,20);
+        site.tF=data(:,21);
+        site.Rho_UA=data(:,22);
+    else
+        disp('No track data detected');
+        site='Error';
+    end
+end
+
+function site = readLaserData(fileName)
+   data = readmatrix(fileName);
+   site.doy = data(:,1);
+   site.sod = data(:,2);
+   site.reedswithc = data(:,3);
+   site.time = data(:,4);
+end
+
+function [datatgtA,datatgtB,datatgtC] = minicoResults(minico,offset,laserData,Srange,plotStadistics)
+    for i = 1:length(minico)
+        switch mod(i,3)
+            case 1
+                tgt='A';
+            case 2
+                tgt='B';
+            case 0
+                tgt='C';
+            otherwise
+                break;
+        end
+        a(i)= processData(laserData,minico(i).targets,Srange,minico(i).delay, ...
+            minico(i).startTimeSod,minico(i).stopTimeSod,tgt); 
+    end
+    %Combine Readed data
+    tgtA=[];
+    tgtB=[];
+    tgtC=[];
+    for i=1:length(a)
+        b=struct2table(a(i));
+        switch mod(i,3)
+            case 1
+                tgtA=[tgtA;b];
+            case 2
+                tgtB=[tgtB;b];
+            case 0
+                tgtC=[tgtC;b];
+            otherwise
+                break;
+        end
+    end
+    %Transform to struct again
+    datatgtA = table2struct(tgtA,"ToScalar",true);
+    datatgtB = table2struct(tgtB,"ToScalar",true);
+    datatgtC = table2struct(tgtC,"ToScalar",true);
+    %Perform stadistical Analisys;
+    [datatgtA.mean,datatgtA.Sr] = dataAnalitics(datatgtA.dist,plotStadistics,1000);
+    [datatgtB.mean,datatgtB.Sr] = dataAnalitics(datatgtB.dist,plotStadistics,1000);
+    [datatgtC.mean,datatgtC.Sr] = dataAnalitics(datatgtC.dist,plotStadistics,1000);
+    
+    %Filter data each 10 measures
+    samples = 10;
+    coeff = ones(1,samples)/samples;
+    datatgtA.distFilt = filter(coeff, 1, datatgtA.dist);
+    datatgtB.distFilt = filter(coeff, 1, datatgtB.dist);
+    datatgtC.distFilt = filter(coeff, 1, datatgtC.dist);
+    
+    %Get ENU coordinates
+    datatgtA.dN = datatgtA.distFilt(10:end)*cos(minico(1).targets.targetA.elevation+offset.offanglesTarA(2))...
+        *cos(minico(1).targets.targetA.azimuth+offset.offanglesTarA(1));
+    datatgtA.dE = datatgtA.distFilt(10:end)*cos(minico(1).targets.targetA.elevation+offset.offanglesTarA(2))...
+        *sin(minico(1).targets.targetA.azimuth+offset.offanglesTarA(1));
+    datatgtA.dU = datatgtA.distFilt(10:end)*sin(minico(1).targets.targetA.elevation+offset.offanglesTarA(2));
+    datatgtB.dN = datatgtB.distFilt(10:end)*cos(minico(1).targets.targetB.elevation+offset.offanglesTarB(2))...
+        *cos(minico(1).targets.targetB.azimuth+offset.offanglesTarB(1));
+    datatgtB.dE = datatgtB.distFilt(10:end)*cos(minico(1).targets.targetB.elevation+offset.offanglesTarB(2))...
+        *sin(minico(1).targets.targetB.azimuth+offset.offanglesTarB(1));
+    datatgtB.dU = datatgtB.distFilt(10:end)*sin(minico(1).targets.targetB.elevation+offset.offanglesTarB(2));
+    datatgtC.dN = datatgtC.distFilt(10:end)*cos(minico(1).targets.targetC.elevation+offset.offanglesTarC(2))...
+        *cos(minico(1).targets.targetC.azimuth+offset.offanglesTarC(1));
+    datatgtC.dE = datatgtC.distFilt(10:end)*cos(minico(1).targets.targetC.elevation+offset.offanglesTarC(2))...
+        *sin(minico(1).targets.targetC.azimuth+offset.offanglesTarC(1));
+    datatgtC.dU = datatgtC.distFilt(10:end)*sin(minico(1).targets.targetC.elevation+offset.offanglesTarC(2));
+end
