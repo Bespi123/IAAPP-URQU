d86e041f53d4b44e736d2dabab2dea9211895451
unknown <Bespi123>
2022-08-14 10:56:54 -0500

    Update function to estimate azimuth and elevation offsets with the error function

--- TargetOptVar.m
+++ TargetOptVar.m
@@ -1,113 +1,71 @@
+function targetOptVar(lowth,highth,laserData,gnssData,offset) 
+    %% Obtaining initial values 
+    %%%Generate meshgrid                          
+        [elevoff,azimuthoff] = meshgrid(lowth:.01:highth, lowth:.01:highth);                                  
+    %%%Inport GNSS Constants
+        gnssdN   =  mean(gnssData.ENU(:,2));
+        gnssdE   =  mean(gnssData.ENU(:,1));
+        gnssdU   =  mean(gnssData.ENU(:,3));
+    %%%Inport SLR constants
+        slrRange =  mean(laserData.rawRange);
+        gnssRange = mean(gnssData.range);
+        elev     =  mean(laserData.elev);
+        az       =  mean(laserData.azim);
+    %%%Calculate rectangular coordinates and modulus
+        [dN,dE,dU]=sph2cart(az+azimuthoff,elev+elevoff,slrRange);  
+        slrRangeExp=(dN.^2+dE.^2+dU.^2).^(1/2);
+    %%% Add offsets
+        dN=dN-offset.slr.ecc_une(2);
+        dE=dE-offset.slr.ecc_une(3);
+        dU=dU-offset.slr.ecc_une(1);
+    %%% Calculate function error
+        Err=((gnssdN-dN).^2+(gnssdE-dE).^2+(gnssdU-dU).^2).^(1/2);
+    %%% Plot Solution
+        figure()
+        s=surf(elevoff,azimuthoff,Err);
+        s.EdgeColor = 'none';
+        a=strcat('Target ',laserData.name);
+        title(a);
+        xlabel('Elevation Offset (rad)');
+        ylabel('Azimuth Offset (rad)');
+        zlabel('Error (m)');    
+    %%% Calculate offsets that minimize error
+        [Errmin,i]=min(Err(:));
+    
+    %% Minimization Problem
+    options = optimoptions(@fminunc,'Display','iter','Algorithm','quasi-newton','StepTolerance',1e-20);
+    x0 = [azimuthoff(i),elevoff(i)];
+    fun = @slrModel;
+    [x,fval,exitflag,output] = fminunc(fun,x0,options);
+    
+    %% Recalculate Values
+    %%%Calculate rectangular coordinates and modulus
+        [dN,dE,dU]=sph2cart(az+x(1),elev+x(2),slrRange);  
+        Errmin = ((gnssdN-dN).^2+(gnssdE-dE).^2+(gnssdU-dU).^2).^(1/2);
+    %%% Add offsets
+        dN=dN-offset.slr.ecc_une(2);
+        dE=dE-offset.slr.ecc_une(3);
+        dU=dU-offset.slr.ecc_une(1);
+    %%% Calculate range    
+        slrRangeExp=(dN.^2+dE.^2+dU.^2).^(1/2);
 
-close all
-%%Plot optval
-[Elev,Azim] = meshgrid(0:0.01:6,0:0.01:6);
-Elev=-5*pi/180:0.00000001:5*pi/180;
+    %% Show results
+    fprintf('dN err: %f, dE err: %f, dU err: %f, Min err: %f, azimuth offset: %f, Elevation offset: %f\n', ...
+        (gnssdN-dN),(gnssdE-dE),(gnssdU-dU),Errmin,x(1)*180/pi,x(2)*180/pi);
+    fprintf('Slr Range: %f, GNSS Range %f, Error: %f\n', mean(mean(slrRangeExp)), ...
+        gnssRange ,mean(mean(slrRangeExp))-gnssRange )
+    fprintf('Slr az: %f, GNSS az: %f\n',(az+x(1))*180/pi, ...
+        mean(gnssData.az)*180/pi)
+    fprintf('Slr elev: %f, GNSS elev: %f\n',(elev+x(2))*180/pi, ...
+        mean(gnssData.elev)*180/pi)
 
-%%Problem Constants
-zN=mean(arelRef.track.ENUtarcArel(:,2));
-zE=mean(arelRef.track.ENUtarcArel(:,1));
-zU=mean(arelRef.track.ENUtarcArel(:,3));
-ab=mean(datatgtC.distFilt(10:end));
-%dN=ab*cos(minico(1).targets.targetA.elevation+Elev)*cos(minico(1).targets.targetA.azimuth+offset.offanglesTarA(1));
-%dE=ab*cos(minico(1).targets.targetA.elevation+Elev)*sin(minico(1).targets.targetA.azimuth+offset.offanglesTarA(1));
-dN=ab*cos(minico(1).targets.targetC.elevation+Elev)*cos(minico(1).targets.targetC.azimuth+offset.offanglesTarC(1));
-dE=ab*cos(minico(1).targets.targetC.elevation+Elev)*sin(minico(1).targets.targetC.azimuth+offset.offanglesTarC(1));
-dU=ab*sin(minico(1).targets.targetC.elevation+Elev);
-
-Err=abs(zN-dN)+abs(zE-dE)+abs(zU-dU);
-figure()
-plot(Elev,Err)
-%surf(Elev,Azim,dN)
-%figure()
-%surf(Elev,Azim,dE)
-%figure()
-%surf(Elev,Azim,Err)
-[Errmin,i]=min(Err(:));
-%x=[Elev(i)*180/pi,Azim(i)*180/pi], 
-x=Elev(i)*180/pi
-Errmin
-
-figure()
-plot(Elev,abs(zU-dU),Elev,20*1E-2*ones(1,length(dU)))
-% % %%Plot optval
-% % %[Elev,Azim] = meshgrid(-5*pi/180:0.001:5*pi/180,-5*pi/180:0.1:5*pi/180);
-%  Elev=-100*pi/180:0.00001:100*pi/180;
-% % %%Problem Constants
-%  zN=mean(arelRef.track.ENUtarcArel(:,2));
-%  zE=mean(arelRef.track.ENUtarcArel(:,1));
-%  ab=mean(datatgtC.distFilt(10:end));
-%  dN=ab*cos(minico(1).targets.targetC.elevation+Elev).*cos(minico(1).targets.targetC.azimuth+1.3373*pi/180);
-%  dE=ab*cos(minico(1).targets.targetC.elevation+Elev).*sin(minico(1).targets.targetC.azimuth+1.3373*pi/180);
-%  figure
-%  Err=abs(zN-dN)+abs(zE-dE);
-%  plot(Elev,Err)
-% % %surf(Elev,Azim,dN)
-% % %figure()
-% % %surf(Elev,Azim,dE)
-% % %figure()
-% % %surf(Elev,Azim,Err)
-%  [Errmin,i]=min(Err);
-% % % %x=[Elev(i)*180/pi,Azim(i)*180/pi], 
-%   x=Elev(i)*180/pi
-%   Errmin
-
-% %%Plot optval
-% %[Elev,Azim] = meshgrid(-5*pi/180:0.001:5*pi/180,-5*pi/180:0.1:5*pi/180);
-% Elev=-100*pi/180:0.0001:100*pi/180;
-% %%Problem Constants
-% zN=mean(arelRef.track.ENUtarbArel(:,2));
-% zE=mean(arelRef.track.ENUtarbArel(:,1));
-% ab=mean(datatgtB.distFilt(10:end));
-% dN=ab*cos(minico(1).targets.targetB.elevation+Elev).*cos(minico(1).targets.targetB.azimuth-0.1416*pi/180);
-% dE=ab*cos(minico(1).targets.targetB.elevation+Elev).*sin(minico(1).targets.targetB.azimuth-0.1416*pi/180);
-% figure
-% Err=abs(zN-dN)+abs(zE-dE);
-% plot(Elev,Err)
-% % %surf(Elev,Azim,dN)
-% % %figure()
-% % %surf(Elev,Azim,dE)
-% % %figure()
-% % %surf(Elev,Azim,Err)
-% [Errmin,i]=min(Err(:));
-% % %x=[Elev(i)*180/pi,Azim(i)*180/pi], 
-%  x=Elev(i)*180/pi
-%  Errmin
-
-
-%%Plot optval
-%[Elev,Azim] = meshgrid(-3*pi/180:0.0001:5*pi/180,-5*pi/180:0.1:3*pi/180);
-
-% %%Problem Constants
-% zN=mean(arelRef.track.ENUtarcArel(:,2));
-% zE=mean(arelRef.track.ENUtarcArel(:,1));
-% ab=mean(datatgtC.distFilt(10:end));
-% %dN=ab*cos(minico(1).targets.targetC.elevation+Elev).*cos(minico(1).targets.targetC.azimuth+offset.offanglesTarC(1));
-% %dE=ab*cos(minico(1).targets.targetC.elevation+Elev).*sin(minico(1).targets.targetC.azimuth+offset.offanglesTarC(1));
-% dN=ab*cos(minico(1).targets.targetC.elevation+Elev).*cos(minico(1).targets.targetC.azimuth+Azim);
-% dE=ab*cos(minico(1).targets.targetC.elevation+Elev).*sin(minico(1).targets.targetC.azimuth+Azim);
-% %figure()
-% Err=abs(zN-dN)+abs(zE-dE);
-% figure()
-% surf(Elev,Azim,Err)
-% 
-% [Errmin,i]=min(Err(:));
-% x=[Elev(i)*180/pi,Azim(i)*180/pi], Errmin
-
-
-
-%%Create an optimization problem.
-% prob = optimproblem('ObjectiveSense','min');
-% xoptvar = optimvar('xoptvar',1,1,'LowerBound',0);
-% 
-% dN = ab*cos(minico(1).targets.targetA.elevation+xoptvar)...
-%         *cos(minico(1).targets.targetA.azimuth+offset.offanglesTarA(1));
-% prob.Objective =zz-dN;
-% cons1 = xoptvar <= 100;
-% cons2 = xoptvar >= -100;
-% prob.Constraints.cons1 = cons1;
-% prob.Constraints.cons2 = cons2;
-% show(prob);
-% sol0.xoptvar=-3.1098;
-% sol = solve(prob,sol0);
-% sol.xoptvar*180/pi;
+    %% User defined functions
+    function f = slrModel(x)
+        [dNx1,dEx1,dUx1]=sph2cart(az+x(1),elev+x(2),slrRange); 
+        %%Add offsets
+        dNx1=dNx1-offset.slr.ecc_une(2);
+        dEx1=dEx1-offset.slr.ecc_une(3);
+        dUx1=dUx1-offset.slr.ecc_une(1);
+        f = ((gnssdN-dNx1)^2+(gnssdE-dEx1)^2+(gnssdU-dUx1)^2)^(1/2);
+    end
+end
